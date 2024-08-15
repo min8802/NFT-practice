c869c33a1cd49d92012d080fdaf8ce6fbb138015
min8802 <ww330@naver.com>
2024-08-15 17:04:55 +0900

    add site Link

--- vite/src/index.d.ts
+++ vite/src/index.d.ts
@@ -12,4 +12,19 @@ interface NftMetadata {
     }[]
 }
 
+interface SaleNftMetadata extends NftMetadata {
+    price : bigint;
+    tokenOwner : string;
+}
+
+// type a = 
+// {name : string; }
+
+// type b = 
+// {age : number; }
+
+// type c = a & b;
+
+
+//number랑 string 둘다 가진 타입이 됨
 //attributes는 없을 수도 있으니까 optional로 ? 표시
