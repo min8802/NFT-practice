00884d0b202ebb9b0b95e25d2dcc990ee4ed55ab
min8802 <ww330@naver.com>
2024-08-15 17:06:58 +0900

    edit mainPage mt

--- vite/src/pages/Home.tsx
+++ vite/src/pages/Home.tsx
@@ -20,7 +20,7 @@ const Home: FC = () => {
         >Don't miss your chance to own the world's best slime meme coin!
         </Text>
       </Flex>
-      <Flex gap={10} mt={20} justifyContent="center">
+      <Flex gap={10} mt={14} justifyContent="center">
         <Link to="https://x.com/home">
           <Image width={12} height={12} src="https://static.wixstatic.com/media/1f3f2b_8eaff3e5f42b45f88cf11d418e25d5e6~mv2.png/v1/fill/w_160,h_160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/twitter%20(2).png"/>
         </Link>
@@ -34,7 +34,7 @@ const Home: FC = () => {
         <Image width={12} height={12} src="https://static.wixstatic.com/media/1f3f2b_9d0d4341170c4640b3663065417b3700~mv2.png/v1/fill/w_160,h_160,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/uniswap%20(2).png"/>
         </Link>
       </Flex>
-      <Flex mt={20} justifyContent="center">
+      <Flex mt={14} justifyContent="center">
         <Grid
           templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(3, 1fr)"]}
           height={120}
